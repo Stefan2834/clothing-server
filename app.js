b24cf7bbdbfd9bce15a8950612f19abc12b3c5a2
Stefan <iosifs617@gmail.com>
2023-03-29 11:51:04 +0300

    main

--- app.js
+++ app.js
@@ -18,17 +18,7 @@ app.set('view engine', 'jade');
 
 const allowedOrigins = ['http://localhost:3000',
   'www.example2.com'];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-
-}));
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
