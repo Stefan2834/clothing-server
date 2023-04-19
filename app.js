4c61fb85df4426b46bd97a9279389a3a23c178ec
Stefan <iosifs617@gmail.com>
2023-04-19 20:00:25 +0300

    test

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const app = express();
 app.use(cookieParser());
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'https://clothing-shop2834.web.app');
-  // res.setHeader('Access-Control-Allow-Origin', 'http://192.168.100.34:3000') 
   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  // res.setHeader('Access-Control-Allow-Origin', 'http://192.168.100.34:3000') 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
