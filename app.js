7cca51dd3d106a4d73793758871998ac648a66a2
Stefan <iosifs617@gmail.com>
2023-04-19 17:01:18 +0300

    test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const connectRouter = require('./routes/connect');
@@ -16,9 +17,9 @@ const adminRoute = require('./routes/admin')
 const app = express();
 app.use(cookieParser());
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://clothing-shop2834.web.app');
+  // res.setHeader('Access-Control-Allow-Origin', 'https://clothing-shop2834.web.app');
   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
-  // res.setHeader('Access-Control-Allow-Origin', 'http://192.168.100.34:3000') 
+  res.setHeader('Access-Control-Allow-Origin', 'http://192.168.100.34:3000') 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
