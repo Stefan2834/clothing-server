b3fb52cf0140413f2228c738a1b65f3b356c299c
Stefan <iosifs617@gmail.com>
2023-04-19 18:16:10 +0300

    test

--- routes/email.js
+++ routes/email.js
@@ -6,7 +6,7 @@ const db = firebase.database()
 const SibApiV3Sdk = require('sib-api-v3-sdk');
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
 const apiKeyAuth = defaultClient.authentications['api-key'];
-apiKeyAuth.apiKey = process.env.API_KEY;
+apiKeyAuth.apiKey = "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";
 const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi()
 
 
